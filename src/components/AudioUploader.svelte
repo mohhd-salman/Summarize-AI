0a99042a2854093c64cc2ae9711e87c9df1345d4
mohhd-salman <mohhd.salman@gmail.com>
2025-12-29 02:27:43 -0800

    UPDATE: api base url

--- src/components/AudioUploader.svelte
+++ src/components/AudioUploader.svelte
@@ -1,5 +1,5 @@
 <script>
-    const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5000";
+    const API_BASE_URL = process.env.API_BASE_URL;
     import {onMount} from "svelte";
     import LoadingSpinner from "./LoadingSpinner.svelte";
     import {
